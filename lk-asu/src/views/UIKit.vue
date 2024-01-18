332882d6c53f1d9242052e02d879f0ac78976d06
v.stepanyan@magnus-tech.ru <password>
2024-01-18 21:37:27 +0300

    ui kit

---
+++ lk-asu/src/views/UIKit.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="kit">
+    <div class="block colors">
+      <div class="col">
+        <div class="bar bg-white with-border" />
+        <div class="bar bg-black" />
+      </div>
+
+      <div class="col">
+        <div class="bar bg-main" />
+        <div class="bar bg-main-500" />
+        <div class="bar bg-main-400" />
+        <div class="bar bg-main-350" />
+        <div class="bar bg-main-300" />
+        <div class="bar bg-main-200" />
+        <div class="bar bg-main-100" />
+        <div class="bar bg-main-50" />
+      </div>
+
+      <div class="col">
+        <div class="bar bg-additional" />
+        <div class="bar bg-additional-300" />
+        <div class="bar bg-additional-200" />
+        <div class="bar bg-additional-100" />
+      </div>
+
+      <div class="col">
+        <div class="bar bg-positive" />
+        <div class="bar bg-positive-light" />
+      </div>
+
+      <div class="col">
+        <div class="bar bg-attention" />
+        <div class="bar bg-attention-light" />
+        <div class="bar bg-attention-300" />
+        <div class="bar bg-attention-200" />
+      </div>
+
+      <div class="col">
+        <div class="bar bg-negative" />
+        <div class="bar bg-negative-light" />
+        <div class="bar bg-negative-200" />
+      </div>
+
+      <div class="col">
+        <div class="bar bg-purple" />
+        <div class="bar bg-purple-50" />
+      </div>      
+    </div>
+
+    <div class="block fonts">
+      <div class="col">
+        <div class="text-2xl-semibold">{{ text }}</div>
+        <div class="mt-2 text-xl-semibold">{{ text }}</div>
+        <div class="mt-2 text-lg-semibold">{{ text }}</div>
+        <div class="mt-2 text-sm-semibold">{{ text }}</div>
+
+        <div class="mt-2 text-lg-medium-uppercase">{{ text }}</div>
+        <div class="mt-2 text-base-medium-uppercase">{{ text }}</div>
+        <div class="mt-2 text-sm-medium-uppercase">{{ text }}</div>
+        <div class="mt-2 text-xs-medium-uppercase">{{ text }}</div>
+        
+        <div class="mt-2 text-lg-medium">{{ text }}</div>
+        <div class="mt-2 text-base-medium">{{ text }}</div>
+        <div class="mt-2 text-sm-medium">{{ text }}</div>
+        <div class="mt-2 text-xs-medium">{{ text }}</div>
+
+        <div class="mt-2 text-lg-regular">{{ text }}</div>
+        <div class="mt-2 text-base-regular">{{ text }}</div>
+        <div class="mt-2 text-sm-regular">{{ text }}</div>
+        <div class="mt-2 text-xs-regular">{{ text }}</div>
+        <div class="mt-2 text-xxs-regular">{{ text }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style setup lang="scss">
+
+  .kit {
+    .block {
+      display: flex;
+      flex-direction: row;
+      margin-top: 12px;
+
+      .col {
+        display: flex;
+        flex-direction: column;
+
+        .bar {
+          height: 24px;
+          width: 172px;
+
+          &.with-border {
+            border: 1px solid;
+            @apply border-black;
+
+          }
+        }
+
+        .bar + .bar {
+          margin-top: 8px;
+        }
+      }
+
+      .col + .col {
+        margin-left: 12px;
+      }
+    }
+  }
+
+</style>
+
+<script setup lang="ts">
+  const text = 'Съешь ещё этих мягких французских булок, да выпей чаю';
+</script>
+
--- lk-asu/tailwind.config.js
+++ lk-asu/tailwind.config.js
@@ -1,11 +1,183 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+const colors = {
+  transparent: 'transparent',
+  current: 'currentColor',
+  white: '#ffffff',
+  black: '#102030',
+
+  main: {
+    DEFAULT: '#001020',
+    500: '#111111',
+    400: '#111112',
+    350: '#111113',
+    300: '#111114',
+    200: '#111115',
+    100: '#111116',
+     50: '#111117',
   },
-  plugins: [],
-}
+
+  additional: {
+    DEFAULT: '#334455',
+    300: '#334456',
+    200: '#334457',
+    100: '#334458',
+  },
+
+  positive: {
+    DEFAULT: '#1FA244',
+    light: '#DADBDC',
+  },
+
+  attention: {
+    DEFAULT: '#E5AD5F',
+    light: '#FCEFD5',
+    300: '#DDA538',
+    200: '#FFF8DD',
+  },
+
+  negative: {
+    DEFAULT: '#E03B39',
+    light: '#FFD3D3',
+    200: '#FEEEEE',
+  },
+
+  purple: {
+    DEFAULT: '#6D38DD',
+    50: '#F2EEFF',
+  },
+};
+
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+    './node_modules/tw-elements/dist/js/**/*.js',
+  ],
+
+  theme: {
+    extend: {
+      colors,
+      width: {
+        22: '5.5rem',
+        58: '14.5rem',
+        89: '22.25rem',
+        100: '25rem',
+        122: '30.5rem',
+      },
+      minWidth: {
+        22: '5.5rem',
+        80: '20rem',
+        58: '14.5rem',
+      },
+      maxWidth: {
+        150: '37.5rem',
+      },
+      height: {
+        22: '5.5rem',
+      },
+      minHeight: {
+        22: '5.5rem',
+      },
+      margin: {
+        22: '5.5rem',
+      },
+      padding: {
+        22: '5.5rem',
+      },
+      borderWidth: {
+        '1.5px': '1.5px',
+      },
+      fontFamily: {
+        sans: ['Roboto Flex', ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        '2xl-semibold': ['1.5rem', {
+          lineHeight: '2rem',
+          fontWeight: '600',
+        }],
+        'xl-semibold': ['1.25rem', {
+          lineHeight: '1.5rem',
+          fontWeight: '600',
+        }],
+        'lg-semibold': ['1.125rem', {
+          lineHeight: '1.5rem',
+          fontWeight: '600',
+        }],
+        'sm-semibold': ['0.875rem', {
+          lineHeight: '1.25rem',
+          fontWeight: '600',
+        }],
+        'lg-medium-uppercase': ['1.125rem', {
+          lineHeight: '1.5rem',
+          fontWeight: '500',
+          letterSpacing: '0.06em',
+        }],
+        'base-medium-uppercase': ['1rem', {
+          lineHeight: '1.5rem',
+          fontWeight: '500',
+          letterSpacing: '0.06em',
+        }],
+        'sm-medium-uppercase': ['0.875rem', {
+          lineHeight: '1.25rem',
+          fontWeight: '500',
+          letterSpacing: '0.06em',
+        }],
+        'xs-medium-uppercase': ['0.75rem', {
+          lineHeight: '1rem',
+          fontWeight: '500',
+          letterSpacing: '0.06em',
+        }],
+        'lg-medium': ['1.125rem', {
+          lineHeight: '1.5rem',
+          fontWeight: '500',
+        }],
+        'base-medium': ['1rem', {
+          lineHeight: '1.375rem',
+          fontWeight: '500',
+        }],
+        'sm-medium': ['0.875rem', {
+          lineHeight: '1.25rem',
+          fontWeight: '500',
+        }],
+        'xs-medium': ['0.75rem', {
+          lineHeight: '1rem',
+          fontWeight: '500',
+        }],
+        'lg-regular': ['1.125rem', {
+          lineHeight: '1.5rem',
+          fontWeight: '400',
+        }],
+        'base-regular': ['1rem', {
+          lineHeight: '1.375rem',
+          fontWeight: '400',
+        }],
+        'sm-regular': ['0.875rem', {
+          lineHeight: '1.25rem',
+          fontWeight: '400',
+        }],
+        'xs-regular': ['0.75rem', {
+          lineHeight: '1rem',
+          fontWeight: '400',
+        }],
+        'xxs-regular': ['0.625rem', {
+          lineHeight: '0.75rem',
+          fontWeight: '400',
+        }],
+      },
+    },
+    container: {
+      center: true,
+      padding: '2',
+    },
+    screens: {
+      xs: '375px',
+    },
+  },
+
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tw-elements/dist/plugin'),
+  ],
+};
